7c2b6732c7bdeccce36047a457ae64685c31ce41
jhonnyred <73783043+jhonnyred@users.noreply.github.com>
2022-08-14 01:10:35 -0300

    Update contas.py

--- contas.py
+++ contas.py
@@ -1,34 +1,65 @@
-al = 0
-cond = 0
-ener = 0
-inter = 0
-despesas = []
+al = 0.0
+cond = 0.0
+ener = 0.0
+inter = 0.0
+cons = 0.0
+mant = 0.0
+dados = [al, cond, ener, inter, cons, mant]
+#hist = open('Histórico.txt', 'r')
+#dados = open('Dados.txt', 'r')
+def escrever(arquivo, lista):
+    cont = 0
+    for valor in lista:
+        arquivo.write(lista[cont])
+        cont += 1
+def extrair(arquivo, lista):
+    cont = 0
+    for line in arquivo:
+        line = line.rstrip()
+        L = int(line)
+        lista[cont] = L
+        cont += 1
+    return lista
+def defina(a, b):
+    a = input('Qual valor para ' + b + ':')
+    return a
+def pula_linha(NLi):
+    while NLi != 0:
+        print('\n')
+        NLi = NLi - 1
+    pass
 while True:
-	print("\n(A)Aluguel:", al,"(C)Condominio:", cond, "(E)Energia:", ener, "(I)Internet:", inter)
-	w = input("Qual valor deseja alterar?(digite 'Q' para calcular)\n").upper()
-	if w == "A":
-		al = input("Digite um valor para o aluguel: ")
-		despesas.append(al)
-	elif w == "C":
-		cond = input("Digite um valor para o condominio: ")
-		despesas.append(cond)
-	elif w == "E":
-		ener = input("Digite um valor para a energia: ")
-		despesas.append(ener)
-	elif w == "I":
-		inter = input("Digite um valor para a internet: ")
-		despesas.append(inter)
-	elif w == "Q":
-		soma = float(al) + float(cond) + float(ener) + float(inter)
-		print("\n Aluguel: ", al, "\n", "Condominio: ", cond, "\n", "Energia: ", ener, "\n", "Internet: ", inter, "\n")
-		print("Soma das despesas: ", "%.2f"%soma)
-		k = input("Deseja dividir a soma?(S/N) ").upper()
-		if k == "S":
-			k = input("Digite o número das partes a ser dividida: ")
-			k = float(k)
-			div = soma / k
-			print("\n Total: ", "%.2f"%soma, "\n", "Para cada parte: ", "%.2f"%div)
-			break
-		elif k == "N":
-			print("Total: ", "%.2f"%soma)
-			break
+    #print(última linha do histórico com as listas ou dicionários)
+    pula_linha(5)
+    print('==============================================================')
+    entr = input('\n(B) Boletos  || (N) Notas fiscais  ||  (H) Histórico \n ==>  ').upper()
+    if entr == 'B':
+        while True:
+            pula_linha(20)
+            print("\n(A) Aluguel:", al,"|| (C) Condominio:", cond, "|| (E)Energia:", ener, "|| (I)Internet:", inter, '||  (S) SALVAR  ||  (Q) SAIR')
+            w = input("Qual valor deseja alterar? \n").upper()
+            if w == 'A':
+                al = defina(al, 'aluguel')
+                dados[0] = al
+            elif w == 'C':
+                cond = defina(cond, 'condominio')
+                dados[1] = cond
+            elif w == 'E':
+                ener = defina(ener, 'energia')
+                dados[2] = ener
+            elif w == 'I':
+                inter = defina(inter, 'internet')
+                dados[3] = inter
+            elif w == 'S':
+                file = open('dados.txt', 'w')
+                cont = 0
+                for valor in dados:
+                    valor = str(dados[cont])
+                    file.write(valor + '\n')
+                    cont += 1
+            elif w == 'Q':
+                break
+#    elif entr == "N":
+#        t = input('(C) Registrar Consumo || (M) Registrar Mantimento').upper()
+#        if t == 'C':
+#        elif t == 'M': 
